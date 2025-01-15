24976d736c56318f40939bf29aa9493b479c9cf7
davidvera13 <david.vera@9online.fr>
2025-01-15 22:47:19 +0100

    feat(typescript): run on brower

---
+++ 01-typescript-bootcamp-beginner-to-advanced(2024edition)/01-fundamentals/03-run-on-browser.js
@@ -0,0 +1,10 @@
+// 1. Compile: tsc 03-run-on-browser.ts
+// 2. add web server (package.json) : npm init
+// 3. run: npm install lite-server
+// 4. run npm start
+var courseName = 'Learning typescript';
+function printCourseName(name) {
+    // we assume name is a string ...
+    console.log('The course name is ' + courseName.toUpperCase());
+}
+printCourseName(courseName);
