f6280874409c251287df7d292862cead9394a2b3
davidvera13 <david.vera@9online.fr>
2025-01-15 22:48:59 +0100

    feat(typescript): any

---
+++ 01-typescript-bootcamp-beginner-to-advanced(2024edition)/02-typescript-type-system/06-any.ts
@@ -0,0 +1,22 @@
+// let variable: any;
+// console.log("type of " + typeof variable);
+// variable = 10;
+// console.log("type of " + typeof variable);
+// variable = "some new value";
+// console.log("type of " + typeof variable);
+// variable = true;
+// console.log("type of " + typeof variable);
+// variable = { name: "john wick"};
+// console.log("type of " + typeof variable);
+// variable = [1, 2, 3, 5, 7, 11];
+// console.log("type of " + typeof variable);
+//
+// let anyArr: any[] = [10, 30, "fifty", false, {name: "john"}];
+// console.log("type of " + typeof anyArr);
+
+
+function printCourse(title: string, lessonCount: number) {
+    console.log(`Title: ${title}, lessons count: ${lessonCount}`)
+}
+//printCourse(19, "TypeScript...");
+printCourse("TypeScript...", 19);
