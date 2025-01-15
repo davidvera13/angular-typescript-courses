62af5f7a2d2542fc18823942a03100ca3636cb56
davidvera13 <david.vera@9online.fr>
2025-01-15 22:49:30 +0100

    feat(typescript): non null assertion

---
+++ 01-typescript-bootcamp-beginner-to-advanced(2024edition)/02-typescript-type-system/08-non-null-assertion.js
@@ -0,0 +1,8 @@
+var courseId;
+// let courseId: number;
+// TypeError: Cannot read properties of undefined (reading 'toString')
+//courseId.toString();
+// we assert courseId IS NOT null
+courseId.toString();
+// error TS2322: Type 'null' is not assignable to type 'number'.
+courseId = null;
---
+++ 01-typescript-bootcamp-beginner-to-advanced(2024edition)/02-typescript-type-system/08-non-null-assertion.ts
@@ -0,0 +1,9 @@
+let courseId: number | null;
+// let courseId: number;
+// TypeError: Cannot read properties of undefined (reading 'toString')
+//courseId.toString();
+
+// we assert courseId IS NOT null
+courseId!.toString();
+// error TS2322: Type 'null' is not assignable to type 'number'.
+courseId = null;
