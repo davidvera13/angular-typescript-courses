b05706afd3635ffabf4455f22a98f883e01ce316
davidvera13 <david.vera@9online.fr>
2025-05-13 07:29:35 +0200

    feat(angular signals): signal introduction startup project

---
+++ 04-angular-signals-in-depth(Angular-19)/01-introduction-to-angular-signals/src/app/components/lessons/lesson-detail/lesson-detail.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+import {ReactiveFormsModule} from "@angular/forms";
+
+@Component({
+    selector: 'lesson-detail',
+    imports: [
+        ReactiveFormsModule
+    ],
+    templateUrl: './lesson-detail.component.html',
+    styleUrl: './lesson-detail.component.scss'
+})
+export class LessonDetailComponent {
+
+}
