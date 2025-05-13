107910d41de98a46442f5e9c6e58bcbed0394d6f
davidvera13 <david.vera@9online.fr>
2025-05-13 07:29:35 +0200

    fix(angular signals): signal introduction startup project

---
+++ 04-angular-signals-in-depth(Angular-19)/01-introduction-to-angular-signals/src/app/components/courses-card-list/courses-card-list.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+import {RouterLink} from "@angular/router";
+
+
+@Component({
+    selector: 'courses-card-list',
+    imports: [],
+    templateUrl: './courses-card-list.component.html',
+    styleUrl: './courses-card-list.component.scss'
+})
+export class CoursesCardListComponent {
+
+}
