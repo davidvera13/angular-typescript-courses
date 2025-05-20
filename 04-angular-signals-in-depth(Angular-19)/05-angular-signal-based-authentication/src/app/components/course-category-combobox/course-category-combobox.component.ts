bcb7b2ac9a225f10a6cd2545abd0aaaa36c63806
davidvera13 <david.vera@9online.fr>
2025-05-20 20:21:55 +0200

    feat(angular signals): authentication using signals startup project

---
+++ 04-angular-signals-in-depth(Angular-19)/05-angular-signal-based-authentication/src/app/components/course-category-combobox/course-category-combobox.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'course-category-combobox',
+  standalone: true,
+  imports: [],
+  templateUrl: './course-category-combobox.component.html',
+  styleUrl: './course-category-combobox.component.scss'
+})
+export class CourseCategoryComboboxComponent {
+
+
+}
---
+++ 04-angular-signals-in-depth(Angular-19)/05-angular-signal-based-authentication/src/app/components/resource-demo/resource-demo.component.ts
@@ -0,0 +1,37 @@
+import {Component, effect, inject, resource, signal} from "@angular/core";
+import {environment} from '../../../environments/environment';
+import {Lesson} from '../../models/lesson.model';
+
+
+@Component({
+  selector: 'resource-demo',
+  templateUrl: './resource-demo.component.html',
+  styleUrls: ['./resource-demo.component.scss'],
+  imports: []
+})
+export class ResourceDemoComponent {
+
+  env = environment;
+
+  search = signal<string>('');
+
+  lessons = signal<Lesson[]>([])
+
+  constructor() {
+    effect(() => {
+      console.log('searching lessons:', this.search() );
+    })
+  }
+
+  searchLessons(search: string) {
+    this.search.set(search);
+  }
+
+  reset() {
+
+  }
+
+  reload() {
+
+  }
+}
