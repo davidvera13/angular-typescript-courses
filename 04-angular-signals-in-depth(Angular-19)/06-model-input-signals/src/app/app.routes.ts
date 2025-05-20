2a96bd20251813b86b7135ad6f0376994cfc0dc5
davidvera13 <david.vera@9online.fr>
2025-05-20 21:22:50 +0200

    fix(angular signals): model inputs signals startup project

---
+++ 04-angular-signals-in-depth(Angular-19)/06-model-input-signals/src/app/app.routes.ts
@@ -0,0 +1,35 @@
+import {Routes} from '@angular/router';
+import {HomeComponent} from "./components/home/home.component";
+import {LoginComponent} from "./components/login/login.component";
+import {LessonsComponent} from "./components/lessons/lessons.component";
+import {ResourceDemoComponent} from "./components/resource-demo/resource-demo.component";
+import {LinkedSignalDemoComponent} from "./components/linked-signals-demo/linked-signal-demo.component";
+import {authGuard} from './guards/auth.guard';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    canActivate: [authGuard]
+  },
+  {
+    path: "login",
+    component: LoginComponent
+  },
+  {
+    path: "lessons",
+    component: LessonsComponent
+  },
+  {
+    path:"shopping-cart",
+    component: LinkedSignalDemoComponent
+  },
+  {
+    path: "resource-demo",
+    component: ResourceDemoComponent
+  },
+  {
+    path: '**',
+    redirectTo: '/'
+  }
+];
