38e1dfd9775bcba8c9d5b1dd69b1dcdd47064af1
davidvera13 <david.vera@9online.fr>
2025-05-20 07:39:02 +0200

    feat(angular signals): loading indicator using signals startup project

---
+++ 04-angular-signals-in-depth(Angular-19)/03-angular-signal-based-loading-indicator/src/app/components/loading/loading.component.ts
@@ -0,0 +1,13 @@
+import {Component } from "@angular/core";
+
+@Component({
+    selector: "loading",
+    templateUrl: "./loading.component.html",
+    styleUrls: ["./loading.component.scss"],
+    imports: []
+})
+export class LoadingIndicatorComponent {
+
+
+
+}
