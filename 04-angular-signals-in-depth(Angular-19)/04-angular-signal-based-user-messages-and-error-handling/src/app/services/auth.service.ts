704607b4cbd1153ab71ea9cee1adce2ea5ed3b59
davidvera13 <david.vera@9online.fr>
2025-05-20 18:56:55 +0200

    feat(angular signals): user messages & error handling using signals startup project

---
+++ 04-angular-signals-in-depth(Angular-19)/04-angular-signal-based-user-messages-and-error-handling/src/app/services/auth.service.ts
@@ -0,0 +1,12 @@
+import {Injectable} from "@angular/core";
+
+
+const USER_STORAGE_KEY = 'user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+
+}
---
+++ 04-angular-signals-in-depth(Angular-19)/04-angular-signal-based-user-messages-and-error-handling/src/app/services/lessons.service.ts
@@ -0,0 +1,33 @@
+import {Routes} from '@angular/router';
+import {HomeComponent} from '../components/home/home.component';
+import {LoginComponent} from '../components/login/login.component';
+import {LessonsComponent} from '../components/lessons/lessons.component';
+import {LinkedSignalDemoComponent} from '../components/linked-signals-demo/linked-signal-demo.component';
+import {ResourceDemoComponent} from '../components/resource-demo/resource-demo.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: "login",
+    component: LoginComponent
+  },
+  {
+    path: "lessons",
+    component: LessonsComponent
+  },
+  {
+    path:"shopping-cart",
+    component: LinkedSignalDemoComponent
+  },
+  {
+    path: "resource-demo",
+    component: ResourceDemoComponent
+  },
+  {
+    path: '**',
+    redirectTo: '/'
+  }
+];
