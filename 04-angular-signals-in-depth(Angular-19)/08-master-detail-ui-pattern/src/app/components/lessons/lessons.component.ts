0a35e9edcbfa13a8da69d3f0f47e0407173ceabb
davidvera13 <david.vera@9online.fr>
2025-05-22 18:43:58 +0200

    feat(angular signals): master detail ui pattern with signals startup project

---
+++ 04-angular-signals-in-depth(Angular-19)/08-master-detail-ui-pattern/src/app/components/lessons/lessons.component.ts
@@ -0,0 +1,15 @@
+import {Component} from '@angular/core';
+
+
+@Component({
+    selector: 'lessons',
+    imports: [],
+    templateUrl: './lessons.component.html',
+    styleUrl: './lessons.component.scss'
+})
+export class LessonsComponent {
+
+
+
+
+}
